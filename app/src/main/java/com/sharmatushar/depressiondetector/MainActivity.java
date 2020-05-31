2c9ff4d8416766f430892711a60563f48db410b2
Tushar <tusharsharmaji23@gmail.com>
2020-06-01 01:35:22 +0530

    Added toolbar and navigation view.

--- app/src/main/java/com/sharmatushar/depressiondetector/MainActivity.java
+++ app/src/main/java/com/sharmatushar/depressiondetector/MainActivity.java
@@ -1,20 +1,66 @@
 package com.sharmatushar.depressiondetector;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initializing values here
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer,
+                null,
+                R.string.app_name,
+                R.string.app_name);
+        NavigationView navigationView = findViewById(R.id.navigation);
+        ImageView toolbarMenuIcon = findViewById(R.id.menu_icon);
+
+        //setting values here
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        //Listeners here
+        drawer.addDrawerListener(toggle);
+        toolbarMenuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.openDrawer(GravityCompat.START);
+            }
+        });
+
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int selectedId = item.getItemId();
+        drawer.closeDrawers();
+        switch (selectedId) {
+            case R.id.help:
+                Toast.makeText(this, "Pending.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return false;
+    }
+
 }
